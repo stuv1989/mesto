21d47cc59e31faece0d07e05ae078fd9b43ca8c4
Sergey Tyurenkov <tyurenkov.89@yandex.ru>
2023-06-05 07:23:20 +0300

    Карточки товара готовы

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,56 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementsContainer = document.querySelector(".elements");
+const elementTemplate = document.querySelector("#template").content;
+
+const elementInfo = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+
+function renderElement({ name, link }) {
+  const cardElement = elementTemplate.querySelector(".element").cloneNode(true);
+  cardElement.querySelector(".element__title").textContent = name;
+  cardElement.querySelector(".element__image").src = link;
+
+  elementsContainer.append(cardElement);
+}
+
+function render() {
+  elementInfo.forEach(renderElement);
+}
+
+render();
+
+
+
 let popupButtonOpen = document.querySelector('.profile__button-edit');
 let popupButtonClose = document.querySelector('.popup__button-close');
 let popupForm = document.querySelector('.popup__form');
@@ -24,3 +77,5 @@ function handleFormSubmit(evt) {
 popupButtonOpen.addEventListener('click', (openPopup));
 popupButtonClose.addEventListener('click', () => tooglePopup(popup));
 popupForm.addEventListener('submit', handleFormSubmit);
+
+
